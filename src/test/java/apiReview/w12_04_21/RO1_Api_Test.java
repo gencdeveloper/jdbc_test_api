f24464a9f1693f1b45ae13e0c45cb09388391cf7
Yunus Emre <yunusozudogru34@gmail.com>
2022-01-31 21:34:39 +0100

    Review Test two

--- src/test/java/apiReview/w12_04_21/RO1_Api_Test.java
+++ src/test/java/apiReview/w12_04_21/RO1_Api_Test.java
@@ -14,17 +14,43 @@ public class RO1_Api_Test {
     String Spartans = "http://52.206.11.157:8000";
 
 
-    //get me all employees from HR api
-    @Test
+
+    @Test //get me all employees from HR api
     public void testOne(){
         //Response body olusturmak icin hazir kodu yaziyoruz.
         //Yukarida emin ol restassured paketlerinin yuklendigine
         Response response = RestAssured.given().accept(ContentType.JSON)
                 .when().get(hrUrl+"/employees");
 
-       response.prettyPrint();
-        Assert.assertEquals(response.statusCode(),200);
-        Assert.assertEquals(response.contentType(),"application/json");
+       response.prettyPrint(); // response body deki prety kismi
+        Assert.assertEquals(response.statusCode(),200); // burada sonucumuzu assert ettik statustCODE ile
+        Assert.assertEquals(response.contentType(),"application/json"); //burada da bekledigimiz basligi assert ettik
     }
+
+
+    @Test //get me all regions from HR api
+    public void testTwo(){
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when().get(hrUrl+"/regions");
+
+        //response.prettyPrint();
+        Assert.assertEquals(response.statusCode(),200); // burada sonucumuzu assert ettik statustCODE ile
+        Assert.assertEquals(response.contentType(),"application/json"); //burada da bekledigimiz basligi assert ettik
+
+        //does the response have "Europe" in it?
+        Assert.assertTrue(response.body().asString().contains("Europe"));
+
+
+        //does my response have the header name "Date" in it
+        Assert.assertTrue(response.headers().hasHeaderWithName("Date"));
+
+
+        //Transfer-Encoding = chunked / header ile ana headrlerimiza gittik ve chunked degerimizi aldik.
+        Assert.assertEquals(response.header("Transfer-Encoding"), "chunked");
+
+    }
+
+
 
 }
